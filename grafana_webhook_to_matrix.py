aaa37db1fec2eeaabf4d4938a3371954edb5c051
Remi Rampin <remi@rampin.org>
2024-05-14 22:10:48 -0400

    Get target information from Authorization header

--- grafana_webhook_to_matrix.py
+++ grafana_webhook_to_matrix.py
@@ -1,12 +1,11 @@
 import aiohttp
 from aiohttp import web
+import base64
 from datetime import datetime
 import os
 
 
-homeserver = os.environ['MATRIX_HOMESERVER']
-access_token = os.environ['MATRIX_ACCESS_TOKEN']
-room = os.environ['MATRIX_ROOM']
+secret = os.environ['SECRET_TOKEN']
 
 
 _last_timestamp = 0
@@ -24,7 +23,7 @@ def unique_number():
     return timestamp * 100 + _counter
 
 
-async def send_message(text):
+async def send_message(homeserver, access_token, room, text):
     async with aiohttp.ClientSession() as http:
         txid = unique_number()
         res = await http.put(
@@ -40,9 +39,26 @@ async def send_message(text):
 
 
 async def handle_alert(request):
+    try:
+        header = request.headers["authorization"]
+    except KeyError:
+        raise web.HTTPBadRequest()
+    if header.startswith('Bearer '):
+        header = header[7:]
+    try:
+        token, homeserver, access_token, room = header.split('-')
+    except ValueError:
+        raise web.HTTPBadRequest()
+    token = base64.b64decode(token).decode('ascii')
+    homeserver = base64.b64decode(homeserver).decode('ascii')
+    access_token = base64.b64decode(access_token).decode('ascii')
+    room = base64.b64decode(room).decode('ascii')
+    if token != secret:
+        raise web.HTTPForbidden()
+
     body = await request.json()
     title = body['title']
-    await send_message(title)
+    await send_message(homeserver, access_token, room, title)
     return web.Response(text='Ok')
 
 
